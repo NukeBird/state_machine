db529655b7c9ade8e98e898f1dcfb70a608fec2b
NukeBird <nukebird.dev@gmail.com>
2017-11-07 14:54:35 +0300

    Reduce StateMachine's API
    There is no need for such varienty of push_state functions.
    push_state(const StateRef&) is OK :)

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -6,11 +6,6 @@ void StateMachine::push_state(const StateRef& state)
 	stack.emplace(state);
 }
 
-void StateMachine::push_state(State* state)
-{
-	push_state(StateRef(state));
-}
-
 StateRef StateMachine::pop_state()
 {
 	auto state = get_current_state();
